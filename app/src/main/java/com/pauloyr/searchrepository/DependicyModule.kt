2ecb55d0c44056b4a450d10483c37f197c6492a4
Paulo Rocha <pauloyr@outlook.com>
2021-06-02 15:23:46 -0300

    Montando modulo para as injenções de depencias

--- app/src/main/java/com/pauloyr/searchrepository/DependicyModule.kt
+++ app/src/main/java/com/pauloyr/searchrepository/DependicyModule.kt
@@ -1,17 +1,25 @@
 package com.pauloyr.searchrepository
 
+import com.pauloyr.searchrepository.data.GitHubRepository
+import com.pauloyr.searchrepository.data.GitHubRepositoryImpl
 import com.pauloyr.searchrepository.data.GitLabRepository
 import com.pauloyr.searchrepository.data.GitLabRepositoryImpl
+import com.pauloyr.searchrepository.data.api.GitHub
 import com.pauloyr.searchrepository.data.api.GitLab
 import com.pauloyr.searchrepository.data.network.Service
 import com.pauloyr.searchrepository.presenter.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val mealServiceModule = module {
+val searchServiceModule = module {
 
-    single { Service().buildApi(GitLab::class.java, get(), "") }
+    single { Service().buildApi(GitLab::class.java, get(), "https://gitlab.com/api/v4/") }
 
-    single { GitLabRepositoryImpl(get()) }
+    single { Service().buildApi(GitHub::class.java, get(), "https://api.github.com") }
 
-    single { MainViewModel(get()) }
+    single<GitLabRepository>{ GitLabRepositoryImpl(get()) }
+
+    single<GitHubRepository> { GitHubRepositoryImpl(get()) }
+
+    viewModel { MainViewModel(gitHubRepository =  get(), gitLabRepository =  get()) }
 }
