39a246496671df7b3aa137ea8556fed0fb14dca8
Paulo Rocha <pauloyr@outlook.com>
2021-06-02 15:23:01 -0300

    Adicionando criterio que as variaveis podem ser nulas

--- app/src/main/java/com/pauloyr/searchrepository/domain/Repository.kt
+++ app/src/main/java/com/pauloyr/searchrepository/domain/Repository.kt
@@ -1,12 +1,11 @@
 package com.pauloyr.searchrepository.domain
 
-import com.google.gson.annotations.SerializedName
 import java.util.*
 
 data class Repository(
-    val description: String,
-    val name: String,
-    val lastActivityAt: Date,
-    val forks: Int,
-    val start: Int,
+    val description: String?,
+    val name: String?,
+    val lastActivityAt: Date?,
+    val forks: Int?,
+    val start: Int?,
 )
