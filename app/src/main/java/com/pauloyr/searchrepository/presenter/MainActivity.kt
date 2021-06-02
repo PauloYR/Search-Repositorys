f0784928cc8a0e6a9e71d905596b1fe25f50f69a
Paulo Rocha <pauloyr@outlook.com>
2021-06-02 15:24:34 -0300

    Adicionando um teste para chamada da api

--- app/src/main/java/com/pauloyr/searchrepository/presenter/MainActivity.kt
+++ app/src/main/java/com/pauloyr/searchrepository/presenter/MainActivity.kt
@@ -3,10 +3,16 @@ package com.pauloyr.searchrepository.presenter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pauloyr.searchrepository.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val searchRepositoryViewModel by viewModel<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchRepositoryViewModel.getRepository("Android",1)
     }
 }
