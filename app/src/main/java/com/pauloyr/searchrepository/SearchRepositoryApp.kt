4e339288b076bcbb32ad8ce7efcae645fe6851fd
Paulo Rocha <pauloyr@outlook.com>
2021-06-02 15:24:20 -0300

    Inicializando o koin

---
+++ app/src/main/java/com/pauloyr/searchrepository/SearchRepositoryApp.kt
@@ -0,0 +1,19 @@
+package com.pauloyr.searchrepository
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class SearchRepositoryApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin{
+            androidContext(this@SearchRepositoryApp)
+            androidLogger(Level.NONE)
+            modules(searchServiceModule)
+        }
+    }
+}
