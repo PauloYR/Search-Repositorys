1da9fd02f118177850ceda24983a751b42e0c260
Paulo Rocha <pauloyr@outlook.com>
2021-06-02 15:18:50 -0300

    Implementando uso dá chamada da api e mesclando as listas

--- app/src/main/java/com/pauloyr/searchrepository/presenter/MainViewModel.kt
+++ app/src/main/java/com/pauloyr/searchrepository/presenter/MainViewModel.kt
@@ -4,19 +4,31 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.pauloyr.searchrepository.data.GitHubRepository
 import com.pauloyr.searchrepository.data.GitLabRepository
 import com.pauloyr.searchrepository.domain.Repository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MainViewModel(val gitLabRepository: GitLabRepository) : ViewModel() {
+class MainViewModel(
+    private val gitLabRepository: GitLabRepository,
+    private val gitHubRepository: GitHubRepository
+) : ViewModel() {
     private val _repository = MutableLiveData<List<Repository>>()
     val repository = _repository as LiveData<List<Repository>>
 
-    fun getRepository(search: String,page: Int){
+    fun getRepository(search: String, page: Int) {
         viewModelScope.launch {
-            val listGitLab = gitLabRepository.getRepositorys("projects",search,page)
-            _repository.value = listGitLab.map { gitlab ->
-                gitlab
+            val listGitLab = gitLabRepository.getRepositorys(search, page)
+            val listGibHub = gitHubRepository.getRepositorys(search, page)
+
+            val list: MutableList<Repository> = ArrayList()
+            list.addAll(listGitLab)
+            list.addAll(listGibHub)
+
+            withContext(Dispatchers.Main) {
+                _repository.value = list.toList()
             }
         }
     }
